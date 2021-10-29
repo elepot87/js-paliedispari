6b078b96a6393dc34dfe43f2a5785fb4959a3884
elepot87 <78643839+elepot87@users.noreply.github.com>
2021-10-29 15:48:42 +0200

    function for random number

---
+++ pari e dispari/js/main.js
@@ -0,0 +1,22 @@
+// Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+
+const btnComputer = getRandom();
+console.log(btnComputer);
+
+// btnComputer.addEventListener('click', function {
+
+// })
+
+/**************************
+Funzioni
+**************************/
+
+function getRandom() {
+  const rand = Math.floor(Math.random() * 5) + 1;
+  return rand;
+}
