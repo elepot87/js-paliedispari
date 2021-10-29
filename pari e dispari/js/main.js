17ca553db70a0bfa3e346c1c83018b20497752a6
elepot87 <78643839+elepot87@users.noreply.github.com>
2021-10-29 16:25:00 +0200

    try to do ciclo while

--- pari e dispari/js/main.js
+++ pari e dispari/js/main.js
@@ -5,12 +5,35 @@
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
 
-const btnComputer = getRandom();
-console.log(btnComputer);
+//Utente scrive se pari o dispari
+const userChoice = prompt("Scrivi se preferisci pari o dispari");
 
-// btnComputer.addEventListener('click', function {
+// Validazione del dato inserito dall'utente, finché non lo inserisce correttamente
 
-// })
+while (isNaN(userNum) || userNum < 1 || userNum > 5) {
+  let userNum = parseInt(prompt("Inserisci un numero da 1 a 5"));
+  if (!isNaN(userNum) || userNum >= 1 || userNum <= 5) {
+    break;
+  }
+}
+
+console.log(userNum);
+
+// Stampiamo nel DOM la scelta dell'utente
+// const printUserChoice = (document.querySelector(
+//   ".utente"
+// ).innerHTML = `La scelta dell'utente è ${userNum}`);
+
+// Il computer deve generare una funzione che crei un numero random da 1 a 5, richiamando una funzione
+const computerChoice = getRandom();
+
+// Stampiamo nel DOM il numero generato dal computer
+
+const printComputerChoice = (document.querySelector(
+  ".computer"
+).innerHTML = `La scelta del computer è ${computerChoice}`);
+
+// Sommiamo i due numeri
 
 /**************************
 Funzioni
